b72bbdee2311850da59e75c3a0a939c4de5a0bc3
BigleBomb <irfanabyan96@gmail.com>
2017-06-13 23:25:56 +0700

    Fixed register_user.php

---
+++ cms/register_user.php
@@ -0,0 +1,51 @@
+<?php
+include('config.php');
+    if(isset($_POST['nama'])
+            //&& ($_POST['nama']!=null)
+            //&& ($_POST['email']!=null)
+            //&& ($_POST['priv']!=null)
+    ){
+        $nama = $_POST['nama'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $priv = $_POST['priv'];
+        switch($priv){
+            case "Karyawan":
+                $priv = 0;
+                break;
+            case "Atasan":
+                $priv = 1;
+                break;
+            case "Admin":
+                $priv = 2;
+                break;
+            default:
+                $priv = 0;
+                break;
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"$SERVER/register");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+                "nama=$nama&username=$username&email=$email&privilege=$priv");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+        $resp = json_decode($server_output);
+
+        if($resp != null){
+            $message = $resp->message;
+            if ($resp->success==true){
+                $password = $resp->pass;
+
+                echo $message;
+            }
+            else {
+                echo $message;
+            }
+        }
+        else{
+            echo "Could not connect to the server.";
+        }
+    }
+?>
