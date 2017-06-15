952dfceda114600de208fc3b34d209aec0cece61
BigleBomb <irfanabyan96@gmail.com>
2017-06-15 14:32:58 +0700

    Updarted get_user_info

--- cms/get_user_info.php
+++ cms/get_user_info.php
@@ -6,7 +6,7 @@ session_start();
         $id = $_POST['user_id'];
         $ch = curl_init();
         $token = $_SESSION['token'];
-        curl_setopt($ch, CURLOPT_URL,"$SERVER/user/$id?token=$token");
+        curl_setopt($ch, CURLOPT_URL,"$SERVER/user/get/$id?token=$token");
         curl_setopt($ch, CURLOPT_POST, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($ch);
