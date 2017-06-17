6829feeba774440b2400b39ce525ed331c650589
BigleBomb <irfanabyan96@gmail.com>
2017-06-18 03:46:26 +0700

    Updated the controller

--- cms/accept_reimburse.php
+++ cms/accept_reimburse.php
@@ -4,10 +4,13 @@ session_start();
     if(isset($_POST['reimburse_id'])
     ){
         $id = $_POST['reimburse_id'];
+        $reason = $_POST['reason'];
         $ch = curl_init();
         $token = $_SESSION['token'];
         curl_setopt($ch, CURLOPT_URL,"$SERVER/reimburse/accept/$id?token=$token");
-        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+                "reason=$reason");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($ch);
         curl_close ($ch);
