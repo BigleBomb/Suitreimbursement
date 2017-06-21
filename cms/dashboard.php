cd6f225f520bcbd9abcf11a9326c1a5e76cea5ee
chrisnafc <chrisnafc@gmail.com>
2017-06-21 12:43:46 +0700

    Fixed Header Page

--- cms/dashboard.php
+++ cms/dashboard.php
@@ -91,7 +91,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Material Dashboard</a>
+						<a class="navbar-brand" href="#">Dashboard</a>
 					</div>
 					<div class="collapse navbar-collapse">
 						<ul class="nav navbar-nav navbar-right">
--- cms/user.php
+++ cms/user.php
@@ -92,7 +92,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Material Dashboard</a>
+						<a class="navbar-brand" href="#">User Management</a>
 					</div>
 					<div class="collapse navbar-collapse">
 						<ul class="nav navbar-nav navbar-right">
