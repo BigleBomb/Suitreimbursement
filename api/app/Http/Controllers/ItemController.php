e074f4a3a3f5143f45cd5cbcb216a9282b6f87e8
BigleBomb <irfanabyan96@gmail.com>
2017-06-22 13:47:55 +0700

    Added new model "Item". Each item may be assigned to one reimburse.

---
+++ api/app/Http/Controllers/ItemController.php
@@ -0,0 +1,125 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use App\Reimburse;
+use App\Item;
+
+class ItemController extends Controller {
+	
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	public function index(Request $request, $id)
+	{
+		$item = Item::where('reimburse_id', $id)->all();
+
+		$res['success'] = true;
+		$res['result'] = $item;
+		// $i=0;
+		// foreach($item as $itemdata)
+		// {
+		// 	$reimburse = Reimburse::find($itemdata->reimburse()->first()->id);
+		// 	$res['result'][$i]['user_data'] = $user;
+		// 	$i++;
+		// }
+		return response($res);
+	}
+	
+	public function create(Request $request)
+	{
+		$reimburse_id = $request->input('reimburse_id');
+		$date = $request->input('date');
+		$category = $request->input('category');
+		$cost = $request->input('cost');
+		if($request->hasFile('picture'))
+			$file = $request->file('picture');
+		else
+			$file = null;
+
+		$reimburse = Reimburse::where('id', $reimburse_id)->first();
+		if($reimburse){
+			$item = Item::create([
+				'reimburse_id' => $reimburse_id,
+				'date' => $date,
+				'category' => $category,
+				'cost' => $cost,
+			]);
+
+			if($item){
+				$res['success'] = true;
+				$res['message'] = 'Success adding new item';
+
+			}else{
+				$res['success'] = false;
+				$res['message'] = 'Failed adding new item';
+
+				return response($res);
+			}
+
+			$it = Item::where('reimburse_id', $reimburse_id)->first();
+			if($it !=null ){
+				if($file != null){
+					if($file->isValid()){
+						$user_id = $reimburse->user_id;
+						$reimburse_id = $it->reimburse_id;
+						$id = $it->id;
+						$filename = "itemPic".$id.".".$file->getClientOriginalExtension();
+						$it->picture = $filename;
+						$it->save();
+						$destinationPath = "../../cms/images/u$user_id/r$reimburse_id/";
+						$file->move($destinationPath,$filename);
+
+						$res['file'] = "Uploaded successfully";
+
+						return response($res);
+					}
+					else{
+						$res['file'] = "No file attached";
+						return response($res);
+					}
+				}
+			}
+		}
+		else
+		{
+			$res['success'] = false;
+			$res['message'] = 'Invalid reimburse id';
+			
+			return response($res);
+		}
+	}
+
+	// public function update(Request $request, $menu, $id)
+	// {
+	// 	if($request->has(''))
+	// 	if($request->has('name')){
+	// 		$reimburse = Reimburse::find($id);
+	// 		$reimburse->name = $request->input('name');
+	// 		if($reimburse->save()){
+	// 			$res['success'] = true;
+	// 			$res['message'] = 'Success update '.$request->input('name');
+
+	// 			return response($res);
+	// 		}
+	// 	}else{
+	// 		$res['success'] = false;
+	// 		$res['message'] = 'Please fill name Reimburse';
+	// 		return response($res);
+	// 	}
+	// }
+
+	// public function delete(Request $request, $id){
+	// 	$reimburse = Reimburse::find($id);
+	// 	if($reimburse->delete($id)){
+	// 		$res['success'] = true;
+	// 		$res['message'] = "Success deleting Reimburse with id ".$id;
+
+	// 		return response($res);
+	// 	}
+	// }
+}
---
+++ api/database/migrations/2017_06_22_062508_create_item_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('reimburse_id');
+		    $table->date('date');
+		    $table->string('category');
+		    $table->integer('cost');
+			$table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item');
+    }
+}
